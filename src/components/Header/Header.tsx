e76030d9bda93882727a21471cf7bb2d8fe345e1
pavelkhv <pavel.hvatsik@yandex.ru>
2020-06-18 13:27:06 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import logo from "../../assets/img/logo.svg";
+import sunDark from '../../assets/img/icons/sun-dark.svg';
+import moonDark from '../../assets/img/icons/moon-dark.svg';
 
 import "./header.scss";
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <Link to="./" className="header__link">Coins List</Link>
-      <Link to="./conversion" className="header__link">Conversion</Link>
+      <div className="header__wrap">
+        <NavLink to="/" className="logo">
+          <img src={logo} alt="logo" className="logo__icon"/>
+          <span className="logo__text">R-Coins</span>
+        </NavLink>
+
+        <nav className="header__nav">
+          <NavLink to="/" className="header__link" activeClassName="header__link_active" exact >Coins List</NavLink>
+          <NavLink to="/conversion" className="header__link" activeClassName="header__link_active">Conversion</NavLink>
+        </nav>
+
+        <div className="toggle-theme">
+          <img src={sunDark} className="toggle-theme__icon" alt="sun"/>
+          <img src={moonDark} className="toggle-theme__icon" alt="moon"/>
+        </div>
+      </div>
     </header>
   );
 };
